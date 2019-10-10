c855f2b464030ed4710fa9896faa0d5448ea2fdb
saraashleyhs <sara.ashleyhs@gmail.com>
2019-10-09 20:52:20 -0500

    End of class 10/9/19

--- TrelloClone/src/app/app.component.ts
+++ TrelloClone/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TrelloClone';
+names :string[] = ["Jim","Sara", "Steven"];
+show : boolean = true;
+toggle(){
+this.show = !this.show;
 }
+}
+
+
